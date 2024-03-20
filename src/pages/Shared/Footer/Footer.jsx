8ce87a722ded6374398b226e54e680550682ab67
Neaz-mq <neazmorshed666@gmail.com>
2024-03-20 20:26:38 +0600

    navbar and footer polish

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -13,19 +13,19 @@ const Footer = () => {
     return (
         <div className="bg-[#EDEEF7]">
             <footer className="footer p-10 text-base-content">
-  <aside>
+  <aside className="lg:ml-12">
     <img src="https://prographr.vercel.app/_next/static/media/logo.426fb6b7.svg" alt="" />
-    <p className="text-base font-normal mt-2">Belgari, Fapor, Bogura Sadar, Bogura, Bangladesh<br/>support@prographr.com</p>
+    <p className="text-[#564e77] text-base font-semibold font-['__gellix_0bf537, __gellix_Fallback_0bf537'] mt-2">Belgari, Fapor, Bogura Sadar, Bogura, Bangladesh<br/>support@prographr.com</p>
   </aside> 
- <div className="flex flex-wrap  justify-between lg:gap-40 gap-4 ">
- <nav className="flex flex-col flex-wrap text-base font-['__gellix_0bf537'] font-semibold gap-4">
+ <div className="flex flex-wrap  justify-between lg:gap-24 gap-4 ">
+ <nav className="flex text-[#564e77] flex-col  flex-wrap text-base font-['__gellix_0bf537, __gellix_Fallback_0bf537'] lg:ml-32 lg:-mr-5 font-semibold gap-4">
    
    <a className="link link-hover">Flyers</a>
    <a className="link link-hover">Brochures</a>
    <a className="link link-hover">Logo and Branding</a>
    
  </nav> 
- <nav className="flex flex-col flex-wrap text-base font-['__gellix_0bf537'] font-semibold gap-4">
+ <nav className="flex text-[#564e77] flex-col font-semibold flex-wrap text-base font-['__gellix_0bf537, __gellix_Fallback_0bf537']font-semibold gap-4 lg:ml-6">
   
    <a className="link link-hover">Sales Sheets</a>
    <a className="link link-hover">About</a>
@@ -33,7 +33,7 @@ const Footer = () => {
   
  </nav> 
  
-  <nav className="flex flex-col flex-wrap text-base font-['__gellix_0bf537'] font-semibold gap-4">
+  <nav className="flex text-[#564e77] flex-col flex-wrap text-base font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-semibold gap-4 lg:ml-6">
    
     <a className="link link-hover">Customer Support</a>
     <a className="link link-hover">Terms & Conditions</a>
@@ -43,7 +43,7 @@ const Footer = () => {
 </footer>
  {/* Copyright Section */}
  <div className="lg:mt-4 text-center border-t-2 lg:ml-12 lg:mr-44 ml-10 mr-10" id="footer">
-                <p className="mt-6 pb-8 text-sm lg:text-base font-semibold lg:ml-20">&copy; <span id="currentYear"></span> Prographr All right reserved</p>
+                <p className="mt-6 text-[#564e77] pb-8 text-sm lg:text-base font-semibold lg:ml-20">&copy; <span id="currentYear"></span> Prographr All right reserved</p>
             </div>
         </div>
     );
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
 const NavBar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-[#EDEEF7] py-7">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0}  className="lg:hidden">
+      <div tabIndex={0}  className="lg:hidden ml-7">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content -mt-10 z-[1] p-2 leading-8 shadow bg-base-100 w-52 text-center -ml-2">
-      <a href="/" className="text-xl ml-8 mr-1 mb-5"><img src="https://prographr.vercel.app/_next/static/media/logo.426fb6b7.svg" alt="" /></a>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content -mt-16 z-[1] p-8 leading-8 shadow bg-base-100 w-52 text-center -ml-2 font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-semibold">
+      <a href="/" className="text-xl mt-10 ml-4 mr-1 mb-8"><img src="https://prographr.vercel.app/_next/static/media/logo.426fb6b7.svg" alt="" /></a>
       
       <a>Template</a> 
       <a>Company</a>
@@ -16,21 +16,21 @@ const NavBar = () => {
      
       </ul>
     </div>
-    <a href="/" className="text-xl ml-8 mr-1 lg:block hidden"><img src="https://prographr.vercel.app/_next/static/media/logo.426fb6b7.svg" alt="" /></a>
-    <a href="/" className="text-xl ml-8 mr-1 block lg:hidden"><img src="https://prographr.vercel.app/_next/static/media/icon.87854914.svg" alt="" /></a>
+    <a href="/" className="text-xl ml-20 mr-1 lg:block hidden"><img src="https://prographr.vercel.app/_next/static/media/logo.426fb6b7.svg" alt="" /></a>
+    <a href="/" className="text-xl ml-4 mr-1 block lg:hidden"><img src="https://prographr.vercel.app/_next/static/media/icon.87854914.svg" alt="" /></a>
   </div>
   <div className="navbar-center hidden lg:flex mr-36 -ml-14">
-    <ul className="menu menu-horizontal px-2 lg:flex flex-row gap-10">
+    <ul className="menu text-base menu-horizontal px-2 lg:flex flex-row gap-10 font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#1e1c27] font-semibold">
      <a>Template</a> 
       <a>Company</a>
       <a>Contact Us</a>
     </ul>
   </div>
 
-<div className="navbar-end gap-2 lg:gap-0">
-  <button className="btn bg-transparent font-['__gellix_0bf537'] capitalize hover:bg-[#C8C5F0] rounded-full font-light text-[#5D4987] gap-4 shadow-none px-8 py-4   hover:bg-primary/30 !border-[#5D4987] lg:mr-14 lg:-ml-7"><span className="">Sign in</span> </button>
+<div className="navbar-end gap-3 lg:gap-0">
+  <button className="btn btn-sm bg-transparent  capitalize hover:bg-[#C8C5F0] rounded-full font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#241e2f] gap-4 shadow-none px-7    hover:bg-primary/30 !border-[#5D4987] lg:mr-14 ml-2 -mr-2 lg:-ml-7"><span>Sign in</span> </button>
 
-  <button className="btn hover:bg-[#7666E3] bg-[#6658C5] font-light capitalize text-white rounded-full gap-4 px-8 py-4 lg:mr-28 lg:-ml-9"><span className="-mt-1">Sign up</span></button>
+  <button className="btn  btn-sm hover:bg-[#7666E3] bg-[#6658C5] font-['__gellix_0bf537, __gellix_Fallback_0bf537'] capitalize text-white rounded-full gap-4 px-6 py-4 lg:mr-28 lg:-ml-9"><span className="-mt-2">Sign up</span></button>
   </div>
 </div>
 </div>
