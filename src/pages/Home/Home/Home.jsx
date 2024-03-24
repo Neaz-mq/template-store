0e121fbe7949e819e2ce2d6cd93e0ab2310f7afa
Neaz-mq <neazmorshed666@gmail.com>
2024-03-24 15:42:39 +0600

    guidline  section work

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from '../Banner/Banner'
 import Creativity from '../Creativity/Creativity';
+import Guideline from '../Guideline/Guideline';
 import Template from '../Template/Template';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
           <Banner></Banner>
           <Template></Template>
           <Creativity></Creativity>
+          <Guideline></Guideline>
         </div>
     );
 };
--- src/pages/Home/Template/Template.jsx
+++ src/pages/Home/Template/Template.jsx
@@ -28,3 +28,22 @@ const Template = () => {
 };
 
 export default Template;
+
+
+// </div>
+{/* <div className="translate-y-24 opacity-0 gsapTrigger2">
+<div className="max-w-[940px] mx-auto grid gap-8 lg:gap-0 grid-cols-1 lg:grid-cols-12 place-items-center mt-16">
+<div className="col-span-2 text-center flex flex-col items-center">
+<div className="border-[10px] shadow-xl shadow-black/20 border-white rounded-full relative">
+<div className="absolute w-[120px] h-[120px] bg-white rounded-full animate-ping"></div>
+
+
+    <img src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fslider.2aa6a74b.png&w=256&q=75" alt="Template" loading="lazy" width="150" height="167" decoding="async" data-nimg="1" className="relative z-10 w-[120px] h-[120px] bg-neutral object-contain p-2 rounded-full decoration-transparent"></img>
+
+    <p className="text-accent mt-8">Your Template</p>
+    
+</div>
+</div>
+</div>
+</div>
+</div> */}
