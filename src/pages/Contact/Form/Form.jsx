3b32cd87a5c36f83c420a6b7682b4706a270bd44
Neaz-mq <neazmorshed666@gmail.com>
2024-03-31 12:27:44 +0600

    comtact page done

--- src/pages/Contact/Form/Form.jsx
+++ src/pages/Contact/Form/Form.jsx
@@ -5,14 +5,14 @@ const Form = () => {
         <div>
            <div className="layout pt-20 pb-48 flex flex-col items-center justify-center">
             
-            <h2 className="text-5xl font-bold text-accent pb-4">How can we help?</h2>
-            <p className="mb-10">If you have any questions, reach out to our team for help</p>
-            <form className="lg:w-[600px] grid grid-cols-2 gap-5">
-            <input className="bg-neutral py-3 px-4 rounded-lg outline-primary" placeholder="Full Name" type="text"/>
-            <input className="bg-neutral py-3 px-4 rounded-lg outline-primary" placeholder="Email" type="email"/>
-            <textarea className="col-span-2 h-[150px] bg-neutral py-3 px-4 rounded-lg outline-primary" placeholder="Message">
+            <h2 className="text-5xl font-bold text-slate-800 font-['__gellix_0bf537, __gellix_Fallback_0bf537']  pb-4">How can we help?</h2>
+            <p className="mb-10 font-medium font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-slate-600">If you have any questions, reach out to our team for help</p>
+            <form className="lg:w-[600px] grid grid-cols-2 gap-5 mx-3 lg:mx-0">
+            <input className="bg-[#EDEEF7] py-3 px-4 rounded-lg outline-[#7868E3]" placeholder="Full Name" type="text"/>
+            <input className="bg-[#EDEEF7] py-3 px-4 rounded-lg outline-[#7868E3]" placeholder="Email" type="email"/>
+            <textarea className="col-span-2 h-[150px] bg-[#EDEEF7] py-3 px-4 rounded-lg outline-[#7868E3]" placeholder="Message">
                 </textarea>
-                <button className="col-span-2 py-4 rounded-lg text-white bg-primary">Send Message</button>
+                <button className="col-span-2 font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium py-4 rounded-lg text-white bg-[#7666E3]">Send Message</button>
                 </form>
                 </div>
         </div>
