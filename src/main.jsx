bf2189c7b1b83cc92b4c87f08bc1b470ee9005b6
Neaz-mq <neazmorshed666@gmail.com>
2024-04-18 20:42:46 +0600

    working AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import {
 import { router } from './Routes/Routes';
 
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <HelmetProvider>
+     <AuthProvider>
+     <HelmetProvider>
       <RouterProvider router={router} />
       </HelmetProvider>
+     </AuthProvider>
   </React.StrictMode>,
 )
