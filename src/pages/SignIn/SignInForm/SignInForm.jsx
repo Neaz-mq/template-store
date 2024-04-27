f20350380d93ad7e7d4774fe729e0d07d99f7d60
Neaz-mq <neazmorshed666@gmail.com>
2024-04-27 10:42:23 +0600

    update SignInForm

--- src/pages/SignIn/SignInForm/SignInForm.jsx
+++ src/pages/SignIn/SignInForm/SignInForm.jsx
@@ -1,15 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './SignInForm.css';
-import { FaGoogle } from 'react-icons/fa';
+import { FaGoogle, FaEye, FaEyeSlash } from 'react-icons/fa';
 import { AuthContext } from '../../../providers/AuthProvider';
 import Swal from 'sweetalert2';
 import { useLocation, useNavigate } from 'react-router-dom';
-const SignInForm = () => {
 
+const SignInForm = () => {
     const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
+    const [passwordVisible, setPasswordVisible] = useState(false);
 
     const handleLogin = event => {
         event.preventDefault();
@@ -18,23 +19,27 @@ const SignInForm = () => {
         const password = form.password.value;
         console.log(email, password);
         signIn(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-            Swal.fire({
-                title: 'User Login Successful.',
-                showClass: {
-                    popup: 'animate__animated animate__fadeInDown'
-                },
-                hideClass: {
-                    popup: 'animate__animated animate__fadeOutUp'
-                }
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire({
+                    title: 'User Login Successful.',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    }
+                });
+                navigate(from, { replace: true });
             });
-            navigate(from, { replace: true });
-        })
-          
-           
-    }
+    };
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    const inputType = passwordVisible ? 'text' : 'password';
 
     return (
         <div className='mb-16'>
@@ -43,7 +48,17 @@ const SignInForm = () => {
                     <h3 className="text-xl font-medium font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-slate-600 mb-6">Sign In</h3>
                     <input className="lg:w-96 w-64 rounded-lg py-2.5 input" placeholder="Email" name="email" type="email" />
                     <br />
-                    <input className="lg:w-96 w-64 rounded-lg py-2.5 input mt-4" placeholder="Password" type="password" name="password" />
+                    <div className="relative">
+                        <input className="lg:w-96 w-64 rounded-lg py-2.5 input mt-4" placeholder="Password" type={inputType} name="password" />
+                        <span className="absolute inset-y-0 right-0 pr-3 flex items-center mt-4">
+                            {passwordVisible ? (
+                                <FaEye onClick={togglePasswordVisibility} className="text-gray-400 cursor-pointer" />
+                            ) : (
+                                
+                                <FaEyeSlash onClick={togglePasswordVisibility} className="text-gray-400 cursor-pointer" />
+                            )}
+                        </span>
+                    </div>
                     <br /><br />
                     <button className="btn bg-[#6658C5] capitalize text-white rounded-full gap-4 w-full mt-3 py-3 shadow-none font-medium font-['__gellix_0bf537, __gellix_Fallback_0bf537'] hover:bg-[#4936c3]">
                         <span className="-mt-1">Sign in</span>
@@ -53,11 +68,11 @@ const SignInForm = () => {
                         </svg>
                     </button>
                     <br /><br />
-                     {/* Google Sign-in Button */}
-                     <button className="btn btn-google bg-white hover:bg-gray-100  capitalize text-black rounded-full gap-4 w-full mt-3 py-3 shadow-none font-medium font-['__gellix_0bf537, __gellix_Fallback_0bf537'] ">
-                    <FaGoogle className="text-xl mr-2 text-red-600" />
-                    Sign in with Google
-                    <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    {/* Google Sign-in Button */}
+                    <button className="btn btn-google bg-white hover:bg-gray-100  capitalize text-black rounded-full gap-4 w-full mt-3 py-3 shadow-none font-medium font-['__gellix_0bf537, __gellix_Fallback_0bf537'] ">
+                        <FaGoogle className="text-xl mr-2 text-red-600" />
+                        Sign in with Google
+                        <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <line x1="5" y1="12" x2="19" y2="12"></line>
                             <polyline points="12 5 19 12 12 19"></polyline>
                         </svg>
@@ -69,7 +84,6 @@ const SignInForm = () => {
                             <polyline points="12 5 19 12 12 19"></polyline>
                         </svg>
                     </button>
-                   
                 </form>
             </div>
         </div>
