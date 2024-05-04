74342733f5b4cf8f3e60e21feeed816d1b240e2e
Neaz-mq <neazmorshed666@gmail.com>
2024-05-04 15:44:49 +0600

    delete function work

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     
     // Calculate total price based on the type of item
     const totalPrice = cart.reduce((total, temp) => {
@@ -11,6 +13,36 @@ const Cart = () => {
         // Check if itemPrice is a valid number, otherwise add 0
         return isNaN(itemPrice) ? total : total + itemPrice;
     }, 0);
+
+
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     
    
     return (
@@ -47,7 +79,9 @@ const Cart = () => {
                                 <td>{temp.name}</td>
                                 <td>${temp.price}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-lg">
+                                    <button
+                                     onClick={() => handleDelete(temp._id)}
+                                    className="btn btn-ghost btn-lg">
                                         <FaTrashAlt className="text-red-600" />
                                     </button>
                                 </th>
