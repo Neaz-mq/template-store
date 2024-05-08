4fbdc820e52b4dac6481bd5c324f77d8eff3a022
Neaz-mq <neazmorshed666@gmail.com>
2024-05-08 10:26:02 +0600

    update navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -154,7 +154,7 @@ const NavBar = () => {
               </span>
               <button
                 onClick={handleLogOut}
-                className="btn btn-sm bg-transparent capitalize hover:bg-[#C8C5F0] rounded-full font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#241e2f] gap-4 shadow-none lg:px-7 px-5 hover:bg-primary/30 !border-[#5D4987]  -ml-12 mr-16 lg:-ml-12 lg:py-5 py-4"
+                className="btn btn-sm bg-transparent capitalize hover:bg-[#C8C5F0] rounded-full font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#241e2f] gap-4 shadow-none lg:px-7 px-5 hover:bg-primary/30 !border-[#5D4987]  -ml-14 mr-20 lg:-ml-14 lg:py-5 py-4"
               >
                 <span className="-mt-2">Sign Out</span>
               </button>
