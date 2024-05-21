77efce77d1157197032a1deb21698d1eca01417d
Neaz-mq <neazmorshed666@gmail.com>
2024-05-21 15:15:33 +0600

    templatedetails page updated

--- src/pages/Dashboard/AddTemplates/AddTemplates.jsx
+++ src/pages/Dashboard/AddTemplates/AddTemplates.jsx
@@ -4,6 +4,7 @@ import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { HiTemplate } from "react-icons/hi";
 
+
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
@@ -11,31 +12,34 @@ const AddTemplates = () => {
     const { register, handleSubmit, reset } = useForm();
     const axiosPublic = useAxiosPublic();
     const axiosSecure = useAxiosSecure();
-
     const onSubmit = async (data) => {
-        console.log(data);
-        const imageFile = { image: data.image[0] };
+        console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] }
         const res = await axiosPublic.post(image_hosting_api, imageFile, {
             headers: {
                 'content-type': 'multipart/form-data'
             }
         });
         if (res.data.success) {
+            // now send the menu item data to the server with the image url
             const templateItem = {
                 name: data.name,
                 category: data.category,
                 price: parseFloat(data.price),
                 details: data.details,
                 image: res.data.data.display_url
-            };
+            }
+            // 
             const templateRes = await axiosSecure.post('/template', templateItem);
-            console.log(templateRes.data);
+            console.log(templateRes.data)
             if (templateRes.data.insertedId) {
+                // show success popup
                 reset();
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
-                    title: `${data.name} is added to the template`,
+                    title: `${data.name} is added to the template.`,
                     showConfirmButton: false,
                     timer: 1500
                 });
@@ -43,13 +47,12 @@ const AddTemplates = () => {
         }
         console.log('with image url', res.data);
     };
-
     return (
-        <div className="p-4">
-            <h2 className="text-2xl lg:text-3xl text-center font-bold mb-4">Add Premium Template</h2>
-            <div className="max-w-lg mx-auto">
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                    <div className="form-control hidden lg:block  w-full">
+        <div>
+            <h2 className="text-3xl text-center font-bold">Add Premium Template</h2>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-control w-full my-6">
                         <label className="label">
                             <span className="label-text">Template Name*</span>
                         </label>
@@ -58,76 +61,60 @@ const AddTemplates = () => {
                             placeholder="Template Name"
                             {...register('name', { required: true })}
                             required
-                            className="input input-bordered w-full text-base md:text-lg" />
+                            className="input input-bordered w-full" />
                     </div>
-
-                    <div className="form-control  lg:hidden w-full">
-                        <label className="label">
-                            <span className="label-text">Template Name*</span>
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            {...register('name', { required: true })}
-                            required
-                            className="input input-bordered w-full text-base md:text-lg" />
-                    </div>
-                    <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
-                        <div className="form-control hidden lg:block w-full">
+                    <div className="flex gap-6">
+                        {/* category */}
+                        <div className="form-control w-full my-6">
                             <label className="label">
                                 <span className="label-text">Category*</span>
                             </label>
                             <select defaultValue="default" {...register('category', { required: true })}
-                                className="select select-bordered w-full text-sm sm:text-base placeholder-sm">
+                                className="select select-bordered w-full">
                                 <option disabled value="default">Select a category</option>
                                 <option value="agency">Agency</option>
                                 <option value="ecommerce">Ecommerce</option>
                                 <option value="business">Business</option>
                                 <option value="portfolio">Portfolio</option>
+
                             </select>
                         </div>
 
-                        <div className="form-control  lg:hidden w-full">
-                            <label className="label">
-                                <span className="label-text">Category*</span>
-                            </label>
-                            <select defaultValue="default" {...register('category', { required: true })}
-                                className="select select-bordered w-full text-sm sm:text-base placeholder-sm">
-                                <option disabled value="default">Select a category</option>
-                                <option value="agency">Agency</option>
-                                <option value="ecommerce">Ecommerce</option>
-                                <option value="business">Business</option>
-                                <option value="portfolio">Portfolio</option>
-                            </select>
-                        </div>
-                        <div className="form-control w-full">
+                        {/* price */}
+                        <div className="form-control w-full my-6">
                             <label className="label">
                                 <span className="label-text">Price*</span>
                             </label>
                             <input
                                 type="number"
-                                step="0.01"
+                                step="0.01" // Allow decimal values
                                 placeholder="Price"
                                 {...register('price', { required: true })}
-                                className="input input-bordered w-full text-sm sm:text-base placeholder-sm" />
+                                className="input input-bordered w-full" />
                         </div>
+
                     </div>
+
+                    {/* recipe details */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Template Details</span>
                         </label>
-                        <textarea {...register('details')} className="textarea textarea-bordered h-24 w-full text-sm sm:text-base placeholder-sm" placeholder="Details"></textarea>
+                        <textarea {...register('details')} className="textarea textarea-bordered h-24" placeholder="Details"></textarea>
                     </div>
-                    <div className="form-control w-full">
+
+                    <div className="form-control w-full my-6">
                         <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
                     </div>
-                    <button className="btn w-full flex items-center justify-center">
-                        Add Template <HiTemplate className="ml-4" />
+
+                    <button className="btn">
+                        Add Template <HiTemplate className="ml-4"></HiTemplate>
                     </button>
                 </form>
             </div>
         </div>
     );
 };
+
 
 export default AddTemplates;
--- src/pages/TemplateDetails/TemplateDetails.jsx
+++ src/pages/TemplateDetails/TemplateDetails.jsx
@@ -23,7 +23,7 @@ const TemplateDetails = () => {
     const { name, _id, price, image } = temp;
     const [templates, setTemplates] = useState([]);
     const [displayedTemplates, setDisplayedTemplates] = useState([]);
-    
+
     const initialDisplayCount = 4;
 
     useEffect(() => {
@@ -35,7 +35,7 @@ const TemplateDetails = () => {
             });
     }, []);
 
-    
+
 
     // Check if there are any templates to display
     if (displayedTemplates.length === 0) {
@@ -100,15 +100,15 @@ const TemplateDetails = () => {
 
     return (
         <div className="lg:ml-20 mb-16">
-            <div className="mt-14 flex flex-row gap-6 ml-2">
-                <div style={{ width: "65%" }}>
+            <div className="mt-14 flex lg:flex-row flex-col gap-6 ml-2">
+                <div className="lg:w-[65%] w-[97%]">
                     <h2 className="text-2xl text-[#2F1C6A] pb-5 font-medium">
                         Premium <strong>Graphics Template</strong>
                     </h2>
-                    <div className="bg-[#EDEEF7] rounded-xl flex items-center justify-center pt-6 pb-4 pl-2 pr-4">
-                        <div className="flex items-center justify-between gap-16 my-8 -mx-20">
-                            <img src={image} className="max-h-[300px] object-contain ml-8 -mr-4" alt="" />
-                            <img src={image} className="max-h-[300px] object-contain -ml-4 mr-4" alt="" />
+                    <div className="bg-[#EDEEF7] rounded-xl flex items-center justify-center pt-6 pb-4 lg:pl-2 lg:pr-4">
+                        <div className="flex items-center justify-between lg:gap-16 gap-10 lg:my-8 lg:-mx-20">
+                            <img src={image} className="lg:max-h-[300px]  max-h-[200px] object-contain ml-8 -mr-4 " alt="" />
+                            <img src={image} className="lg:max-h-[300px]  max-h-[200px] object-contain -ml-4 mr-4" alt="" />
                         </div>
                     </div>
                     <div className="w-full mt-6 flex flex-wrap gap-4 ml-2 lg:ml-0">
@@ -133,12 +133,12 @@ const TemplateDetails = () => {
                                 <div className="flex gap-4 mt-4 ml-2 lg:ml-0">
                                     <img
                                         src="https://i.ibb.co/44VVbr3/11.jpg"
-                                        className="w-[75px] h-[75px] object-contain bg-[#EDEEF7] rounded-lg p-3 cursor-pointer hover:bg-[#7666E3]"
+                                        className="w-[75px] h-[75px] -mt-4 -ml-2 lg:-mt-0 lg:-ml-0  object-contain bg-[#EDEEF7] rounded-lg p-3 cursor-pointer hover:bg-[#7666E3]"
                                         alt=""
                                     />
                                     <img
                                         src="https://i.ibb.co/JrbZzVc/9.jpg"
-                                        className="w-[75px] h-[75px] object-contain bg-[#EDEEF7] rounded-lg p-3 cursor-pointer hover:bg-[#7666E3]"
+                                        className="w-[75px] h-[75px] -mt-4 lg:-mt-0 object-contain bg-[#EDEEF7] rounded-lg p-3 cursor-pointer hover:bg-[#7666E3]"
                                         alt=""
                                     />
                                     {/* Add more images here if needed */}
@@ -155,7 +155,7 @@ const TemplateDetails = () => {
                 </div>
 
                 <div className="flex flex-col items-center mt-12" style={{ width: "60%" }}>
-                    <div style={{ width: "87%", height: "28%" }} className={`border ${selectedTemplate === 'templateCustom' ? 'border-primary' : 'border-gray-400'} rounded-[20px] p-8 -ml-14 mr-9  cursor-pointer`} onClick={() => handleTemplateChange('templateCustom')}>
+                    <div className={`border ${selectedTemplate === 'templateCustom' ? 'border-primary' : 'border-gray-400'} rounded-[20px] p-8 lg:w-[87%] lg:h-[28%] w-[160%] h-[100%] lg:-ml-14 lg:mr-9 ml-40  cursor-pointer`} onClick={() => handleTemplateChange('templateCustom')}>
                         <div className="flex justify-between pb-3">
                             <div className="flex gap-3 font-bold">
                                 <input className="radio radio-primary" type="radio" checked={selectedTemplate === 'templateCustom'} readOnly />
@@ -169,22 +169,22 @@ const TemplateDetails = () => {
                     </div>
 
                     {/* Additional section with the same style in column layout */}
-                    <div className="flex flex-col items-center mt-12">
-                        <div style={{ width: "100%", height: "100%" }} className={`border ${selectedTemplate === 'customizeTemplate' ? 'border-primary' : 'border-gray-400'} rounded-[20px] -ml-9 p-8 mr-12 cursor-pointer`} onClick={() => handleTemplateChange('customizeTemplate')}>
+                    <div className="flex flex-col items-center mt-12 ml-24 lg:ml-0">
+                        <div className={`border ${selectedTemplate === 'customizeTemplate' ? 'border-primary' : 'border-gray-400'} rounded-[20px] lg:-ml-9 ml-16 p-8 lg:mr-12 lg:w-[97%] lg:h-[100%] w-[98%] h-[100%] cursor-pointer`} onClick={() => handleTemplateChange('customizeTemplate')}>
                             <div className="flex justify-between pb-3">
                                 <div className="flex gap-3 font-bold">
                                     <input className="radio radio-primary" type="radio" checked={selectedTemplate === 'customizeTemplate'} readOnly />
-                                    <h2 className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] ">Template + Customization</h2>
+                                    <h2 className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] lg:ml-0 ">Template + Customization</h2>
                                 </div>
                                 <div className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium ">$00</div>
                             </div>
                             <div className="pt-2 border-t font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium">
 
                             </div>
-                            <div className="flex items-center mt-4">
-                                <div className="flex items-center mr-8">
-                                    <div className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium mr-2">Revisions:</div>
-                                    <select className="border rounded-md px-6 py-2" defaultValue="1">
+                            <div className="flex flex-col lg:flex-row items-center mt-4 -ml-6 lg:ml-0">
+                                <div className="flex items-center lg:mr-8 ml-6 lg:ml-0 mb-8 lg:mb-0">
+                                    <div className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium lg:mr-2 mr-8">Revisions:</div>
+                                    <select className="border rounded-md lg:px-6 px-3 py-2 mr-6 -ml-5 lg:mr-0 lg:-ml-0" defaultValue="1">
                                         <option value="1">1</option>
                                         <option value="2">2</option>
                                         <option value="3">3</option>
@@ -193,9 +193,9 @@ const TemplateDetails = () => {
                                     </select>
                                 </div>
                                 <div className="flex items-center">
-                                    <div className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium mr-6">Files:</div>
+                                    <div className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] font-medium lg:ml-3 ml-10 mr-6">Files:</div>
                                     <select
-                                        className="border rounded-md px-3 py-2 -ml-3"
+                                        className="border rounded-md lg:px-3 py-2 lg:-ml-3 mr-10 -ml-3 lg:mr-0"
                                         value=""
                                         onChange={handleFileChange}
                                     >
@@ -209,10 +209,10 @@ const TemplateDetails = () => {
                                     </select>
                                 </div>
                             </div>
-                            <div className="mt-4 flex flex-wrap ml-52">
+                            <div className="mt-4 flex flex-wrap flex-col lg:ml-52 ml-24">
                                 {selectedFiles.map((file, index) => (
                                     <div key={index} className="flex items-center border rounded-md px-4 py-2 mr-2 mb-2">
-                                        <span className="mr-2">{file}</span>
+                                        <span className="">{file}</span>
                                         <button onClick={() => handleRemoveFile(file)}>
                                             <FontAwesomeIcon icon={faTimes} className="text-gray-500" />
                                         </button>
@@ -222,30 +222,30 @@ const TemplateDetails = () => {
                         </div>
                     </div>
                     {/* Add to Cart button */}
-                    <button onClick={handleAddToCart} className="bg-[#7666E3] text-white font-semibold rounded-lg mr-14 -ml-4 w-[32rem] mt-10 hover:bg-[#4c16b1] btn ">
-    Add to Cart
-</button>
+                    <button onClick={handleAddToCart} className="bg-[#7666E3] text-white font-semibold rounded-lg mr-14 lg:-ml-4 lg:w-[32rem] mt-10 hover:bg-[#4c16b1] btn w-[20rem] ml-56 ">
+                        Add to Cart
+                    </button>
 
                     {/* Check more items */}
-                    <button onClick={handleAddToCart} className="bg-gray-300 text-slate-900 font-semibold mr-14 -ml-4 w-[32rem] py-3 rounded-lg mt-4 hover:bg-[#d1bbff] btn ">
-    Check more items
-</button>
+                    <button onClick={handleAddToCart} className="bg-gray-300 text-slate-900 font-semibold mr-14 lg:-ml-4 lg:w-[32rem] py-3 rounded-lg mt-4 hover:bg-[#d1bbff] btn w-[20rem] ml-56 ">
+                        Check more items
+                    </button>
 
 
                 </div>
             </div>
-            <div className="mt-14 flex flex-wrap gap-12">
+            <div className="mt-14 flex flex-wrap lg:flex-row flex-col gap-12">
                 {/* Descriptions */}
-                <div className="flex-1 mb-8 ml-3">
-    <h3 className="text-xl text-[#2F1C6A] font-medium">Descriptions</h3>
-    <p className="text-gray-500 mt-2 overflow-hidden font-medium leading-relaxed" >
-    A business flyer is a versatile and dynamic promotional tool designed to communicate essential information about a business, its products, services, events, or special offers. This tangible marketing collateral is strategically crafted to capture attention, engage the target audience, and generate interest in what the business offers.
-    </p>
-</div>
+                <div className="flex-1 lg:mb-8 ml-3">
+                    <h3 className="text-xl text-[#2F1C6A] font-medium">Descriptions</h3>
+                    <p className="text-gray-500 lg:w-[30rem] mt-2 overflow-hidden font-medium leading-relaxed" >
+                        A business flyer is a versatile and dynamic promotional tool designed to communicate essential information about a business, its products, services, events, or special offers. This tangible marketing collateral is strategically crafted to capture attention, engage the target audience, and generate interest in what the business offers.
+                    </p>
+                </div>
 
 
                 {/* Item Specifications */}
-                <div className="flex-1 mb-8 ml-20 -mr-10">
+                <div className="flex-1 lg:mb-8  lg:-mr-10 ml-3 lg:ml-0">
                     <h3 className="text-xl text-[#2F1C6A] font-medium">Item Specifications</h3>
                     <p className="text-gray-500 mt-2 font-medium leading-relaxed">
                         US Letter & A4 Paper Size<br />
@@ -257,7 +257,7 @@ const TemplateDetails = () => {
                 </div>
 
                 {/* Product Specs */}
-                <div className="flex-1 mb-8 ml-10">
+                <div className="flex-1 lg:mb-8 lg:ml-10 ml-3">
                     <h3 className="text-xl text-[#2F1C6A] font-medium">Product Specs</h3>
                     <p className="text-gray-500 mt-2 font-medium leading-relaxed">
                         Created: Nov 19, 2024<br />
@@ -267,21 +267,21 @@ const TemplateDetails = () => {
                 </div>
 
                 {/* Files Included */}
-                <div className="flex-1 mr-6 ml-7">
+                <div className="flex-1 lg:mr-10 ml-3 lg:ml-0">
                     <h3 className="text-xl text-[#2F1C6A] font-medium">Files Included</h3>
                     <p className="text-gray-500 mt-2 font-medium leading-relaxed">
                         Adobe Photoshop <br />  Adobe Illustrator <br /> Adobe InDesign <br /> Canva <br /> Figma <br /> Microsoft PowerPoint <br /> Microsoft Word
                     </p>
                 </div>
-               
+
             </div>
 
             <div className="layout lg:py-20 py-12 mt-6 ">
                 <div className="flex items-center justify-between mb-10">
                     <h2 className="lg:text-4xl text-xl lg:-mt-8 text-[#2F1C6A] ml-3 lg:ml-4 font-medium">Free <strong>Graphics Templates</strong></h2>
-                    <button className="btn mr-20 ml-4  font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#47435d] bg-transparent capitalize hover:bg-primary/10 rounded-full font-semibold  gap-4 shadow-none p-3 pl-4 border-slate-700"><span className="-mt-1">Printing and Advertising</span> <svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M277.375 85v259.704l119.702-119.702L427 256 256 427 85 256l29.924-29.922 119.701 118.626V85h42.75z"></path></svg></button>
+                    <button className="btn lg:mr-20 mr-2 lg:ml-4  font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#47435d] bg-transparent capitalize hover:bg-primary/10 rounded-full font-semibold  gap-4 shadow-none p-3 pl-4 border-slate-700"><span className="-mt-1">Printing and Advertising</span> <svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M277.375 85v259.704l119.702-119.702L427 256 256 427 85 256l29.924-29.922 119.701 118.626V85h42.75z"></path></svg></button>
                 </div>
-                <div className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 mr-20 " data-aos="lg:fade-right" data-aos-duration="700">
+                <div className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 lg:mr-20 " data-aos="lg:fade-right" data-aos-duration="700">
                     {displayedTemplates.map(item =>
                         <FreeTemplate
                             key={item._id}
@@ -291,10 +291,10 @@ const TemplateDetails = () => {
                 </div>
             </div>
 
-            
+
         </div>
 
-       
+
 
     );
 };
