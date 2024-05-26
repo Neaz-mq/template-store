98259988215c7d5b79b4ec307188da3b854f5d29
Neaz-mq <neazmorshed666@gmail.com>
2024-05-26 11:12:36 +0600

    UPDATE Template Details

--- src/pages/TemplateDetails/TemplateDetails.jsx
+++ src/pages/TemplateDetails/TemplateDetails.jsx
@@ -10,6 +10,8 @@ import { useEffect } from "react";
 import FreeTemplate from "../Shared/FreeTemplate/FreeTemplate";
 
 
+    
+
 const TemplateDetails = () => {
     const [selectedTemplate, setSelectedTemplate] = useState('templateCustom');
     const [selectedFiles, setSelectedFiles] = useState([]);
@@ -23,10 +25,19 @@ const TemplateDetails = () => {
     const temp = useLoaderData();
     const { name, _id, price, image, descriptions, specifications, product, files, revisions } = temp;
 
+    const [templates, setTemplates] = useState([]);
+    const [displayedTemplates, setDisplayedTemplates] = useState([]);
 
 
-
-
+    const initialDisplayCount = 4;
+    useEffect(() => {
+        fetch('http://localhost:5000/free')
+            .then(res => res.json())
+            .then(data => {
+                setTemplates(data);
+                setDisplayedTemplates(data.slice(0, initialDisplayCount));
+            });
+    }, []);
 
 
 
@@ -286,8 +297,27 @@ const TemplateDetails = () => {
             </div>
 
 
+            <div className="layout lg:py-20 py-12 mt-6 ">
+                <div className="flex items-center justify-between mb-10">
+                    <h2 className="lg:text-4xl text-xl lg:-mt-8 text-[#2F1C6A] ml-3 lg:ml-4 font-medium">Free <strong>Graphics Templates</strong></h2>
+                    <button className="btn mr-20 ml-4  font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-[#47435d] bg-transparent capitalize hover:bg-primary/10 rounded-full font-semibold  gap-4 shadow-none p-3 pl-4 border-slate-700"><span className="-mt-1">Printing and Advertising</span> <svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M277.375 85v259.704l119.702-119.702L427 256 256 427 85 256l29.924-29.922 119.701 118.626V85h42.75z"></path></svg></button>
+                </div>
+                <div className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 mr-20 " data-aos="lg:fade-right" data-aos-duration="700">
+                    {displayedTemplates.map(item =>
+                        <FreeTemplate
+                            key={item._id}
+                            item={item}
+                        />
+                    )}
+                </div>
+            </div>
+
 
         </div>
+
+
+        
+        
 
 
 
