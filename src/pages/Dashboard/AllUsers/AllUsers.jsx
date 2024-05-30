704a911c1c8637fcf6739edf5904df82192d3f07
Neaz-mq <neazmorshed666@gmail.com>
2024-05-30 11:20:00 +0600

    UPDATE allusers

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -58,28 +58,58 @@ const AllUsers = () => {
     return (
         <div className="px-4 py-6 sm:px-6 lg:px-8">
             <div className="flex flex-col items-center sm:flex-row sm:justify-between mb-6">
-                <h2 className="text-2xl sm:text-3xl font-semibold">All Users</h2>
-                <h2 className="text-2xl sm:text-3xl font-semibold">Total users: {users.length}</h2>
+                <h2 className="hidden md:block text-2xl sm:text-3xl font-semibold">All Users</h2>
+                <h2 className="text-xl sm:text-3xl font-semibold">Total users: {users.length}</h2>
             </div>
             <div className="overflow-x-auto">
                 <div className="max-w-full overflow-hidden">
                     {users.length > 0 && (
-                        <table className="min-w-full divide-y divide-gray-200">
+                        <div className="grid grid-cols-1 sm:hidden gap-6">
+                            {users.map((user, index) => (
+                                <div key={user._id} className="p-6 border border-gray-200 rounded-lg shadow-md">
+                                    <div className="mb-4 text-sm text-left mr-2 -ml-4">
+                                        <strong>No.:</strong> {index + 1}
+                                    </div>
+                                    <div className="mb-4 text-sm text-left mr-2 -ml-4">
+                                        <strong>Name:</strong> {user.name}
+                                    </div>
+                                    <div className="mb-4 text-sm text-left mr-2 -ml-4">
+                                        <strong>Role:</strong> {user.role === 'admin' ? 'Admin' : (
+                                            <button
+                                                onClick={() => handleMakeAdmin(user)}
+                                                className="btn btn-sm bg-orange-500 text-white mt-2 md:mt-0">
+                                                <FaUsers />
+                                            </button>
+                                        )}
+                                    </div>
+                                    <div className="text-sm text-left mr-2 -ml-2">
+                                        <button
+                                            onClick={() => handleDeleteUser(user)}
+                                            className="btn btn-ghost btn-sm text-red-600">
+                                            <FaTrashAlt />
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    {users.length > 0 && (
+                        <table className="min-w-full divide-y divide-gray-200 hidden sm:table">
                             <thead className="bg-gray-50">
                                 <tr>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No.</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">Email</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
                                 {users.map((user, index) => (
-                                    <tr key={user._id} className="flex flex-col sm:table-row mb-4 sm:mb-0">
+                                    <tr key={user._id}>
                                         <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
                                         <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap hidden sm:table-cell">{user.email}</td>
                                         <td className="px-6 py-4 whitespace-nowrap">
                                             {user.role === 'admin' ? 'Admin' : (
                                                 <button
