fdc5f6ac59df93d2ddc507abedc2a470442ee59b
Neaz-mq <neazmorshed666@gmail.com>
2024-08-17 16:05:21 +0600

    testimonial done

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Swiper from 'swiper';
 import 'swiper/css';
-import './Testimonial.css';
+import './Testimonial.css'
 
 const Testimonial = () => {
 
@@ -9,24 +9,16 @@ const Testimonial = () => {
 
     useEffect(() => {
 
-        const fetchTestimonials = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/testimonials');
-                const data = await response.json();
-                setTestimonials(data);
-            } catch (error) {
-                console.error('Error fetching testimonials:', error);
-            }
-        };
-
-        fetchTestimonials();
-
+        fetch('testimonials.json')
+            .then(res => res.json())
+            .then(data => setTestimonials(data))
         const swiper = new Swiper('.swiper-container', {
             loop: true,
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
             },
+
             on: {
                 slideChange: function () {
                     const bullets = document.querySelectorAll('.swiper-pagination-bullet');
@@ -46,30 +38,48 @@ const Testimonial = () => {
         };
     }, []);
 
-    return (
 
-        <div className='container mx-auto'>
-            <div className='lg:mx-24 lg:mt-8 lg:mb-20 overflow-hidden' style={{ overflowX: 'hidden' }}>
-                <div className="mb-28 lg:mx-24 mx-6">
+    return (
+        
+        <section className='container mx-auto overflow-hidden'>
+            <div className='lg:mx-24 lg:mt-8 lg:mb-20 overflow-hidden'>
+                <div className="mb-28 lg:mx-24">
                     <div className="layout mt-10 mb-14 lg:mt-24">
-                        <h2 className="lg:text-4xl text-3xl text-[#2F1C6A] text-center">Clients <strong>testimonial</strong></h2>
+                        <h2 className="lg:text-4xl text-2xl tablet:text-3xl text-[#2F1C6A] text-center">Clients <strong>testimonial</strong></h2>
                     </div>
 
                     {/* Render Swiper only on smaller screens */}
-                    <div className="swiper-container mt-20 lg:hidden" >
-                        <div className="swiper-wrapper flex gap-10">
+                    <div className="swiper-container mt-16 lg:hidden" >
+                        <div className="swiper-wrapper flex gap-44">
                             {/* Testimonial items */}
                             <div className="swiper-slide">
-                                <div className="max-w-[300px] h-[326px] mt-10 tablet:ml-48 bg-white  text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative mr-6 ml-5">
+                                <article className="w-[230px] h-[470px] tablet:w-[300px] tablet:h-[386px] mt-10 tablet:ml-56 bg-white  text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative mr-2 ml-9">
                                     <div className="p-5 -mt-14 w-fit mx-auto bg-[#7666E3] text-2xl text-white rounded-full"  ><svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m21.95 8.721-.025-.168-.026.006A4.5 4.5 0 1 0 17.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222-.474.197.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.028 7.028 0 0 0-.063-.539zm-11 0-.025-.168-.026.006A4.5 4.5 0 1 0 6.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222c-.301.123-.473.195-.473.195l.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.571 7.571 0 0 0-.064-.537z"></path></svg>
                                     </div>
                                     <div className="h-full flex flex-col justify-between" >
-                                        <p className="pt-4 text-sm">We have been using Prographr marketplace for over 5 years now and has been instrumental in our branding, packaging and all creative design projects. They are always available, has very strong work ethic and integrity. Great quality and exceeds our expectations</p>
-                                        <div className="flex gap-4 pt-4">
-                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[100px] h-[100px] rounded-full" src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.12e50714.png&w=640&q=75" style={{ color: 'transparent' }} />
-                                            <div className="mt-3">
-                                                <h3 className="text-lg font-semibold">Suresh Kanthaswamy</h3>
-                                                <p className="mt-1">Envelor Inc.</p>
+                                        <p className="pt-4 text-[13px] ml-3">We have been using Prographr marketplace for over 5 years now and has been instrumental in our branding, packaging and all creative design projects. They are always available, has very strong work ethic and integrity. Great quality and exceeds our expectations</p>
+                                        <div className="">
+                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[90px] h-[90px] rounded-full ml-11 tablet:ml-20  " src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.12e50714.png&w=640&q=75" style={{ color: 'transparent' }} />
+                                            <div className="mt-8">
+                                                <h3 className="text-sm font-semibold ml-6 mt-4 tablet:ml-14">Suresh Kanthaswamy</h3>
+                                                <p className="text-sm font-normal ml-16 tablet:ml-24">Envelor Inc.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+
+                            <div className="swiper-slide">
+                                <div className="w-[220px] h-[380px] tablet:w-[300px] tablet:h-[350px] mt-10 tablet:ml-10 bg-white text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative  -ml-36">
+                                    <div className="p-5 -mt-14 w-fit mx-auto bg-[#7666E3] text-2xl text-white rounded-full"><svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m21.95 8.721-.025-.168-.026.006A4.5 4.5 0 1 0 17.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222-.474.197.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.028 7.028 0 0 0-.063-.539zm-11 0-.025-.168-.026.006A4.5 4.5 0 1 0 6.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222c-.301.123-.473.195-.473.195l.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.571 7.571 0 0 0-.064-.537z"></path></svg>
+                                    </div>
+                                    <div className="h-full flex flex-col justify-between">
+                                        <p className="pt-4 text-[13px]">I have worked now with Prographr now for nearly a year, with a total of over 50 projects. I can say that with every project, we gets better and better. Reliable and ALWAYS on time!</p>
+                                        <div className="">
+                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[90px] h-[90px] rounded-full ml-11 tablet:ml-20" src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.511feac8.png&w=640&q=75" style={{ color: 'transparent' }} />
+                                            <div className="mt-8">
+                                                <h3 className="text-sm font-semibold ml-14 tablet:ml-24">Kaleb Held</h3>
+                                                <p className="text-sm font-normal ml-14 tablet:ml-24">Industrolux</p>
                                             </div>
                                         </div>
                                     </div>
@@ -77,38 +87,19 @@ const Testimonial = () => {
                             </div>
 
                             <div className="swiper-slide">
-                                <div className="max-w-[280px] h-[326px] mt-10 tablet:ml-40 bg-white text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative mr-16 -ml-2">
+                                <div className="w-[220px] h-[400px] tablet:w-[300px] tablet:h-[326px]  mt-10 bg-white text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative  -ml-[19.5rem] tablet:-ml-[9rem]">
                                     <div className="p-5 -mt-14 w-fit mx-auto bg-[#7666E3] text-2xl text-white rounded-full"><svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m21.95 8.721-.025-.168-.026.006A4.5 4.5 0 1 0 17.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222-.474.197.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.028 7.028 0 0 0-.063-.539zm-11 0-.025-.168-.026.006A4.5 4.5 0 1 0 6.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222c-.301.123-.473.195-.473.195l.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.571 7.571 0 0 0-.064-.537z"></path></svg>
                                     </div>
                                     <div className="h-full flex flex-col justify-between">
-                                        <p className="pt-4 text-sm">I have worked now with Prographr now for nearly a year, with a total of over 50 projects. I can say that with every project, we gets better and better. Reliable and ALWAYS on time!</p>
-                                        <div className="flex gap-4 pt-4">
-                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[100px] h-[100px] rounded-full" src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.511feac8.png&w=640&q=75" style={{ color: 'transparent' }} />
+                                        <p className="pt-4 text-[13px]">Doing real estate business without business card was difficult for me. I found a business card template here and modified by the design owner. The designer did a fantastic job for me. Thanks...</p>
+                                        <div className="ml-20">
+                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[80px] h-[80px] rounded-full -ml-9 tablet:ml-2" src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b967d41d.png&w=640&q=75" style={{ color: 'transparent' }} />
                                             <div className="mt-3">
-                                                <h3 className="text-lg font-semibold">Kaleb Held</h3>
-                                                <p className="mt-1">Industrolux</p>
+                                                <h3 className="text-sm font-semibold -ml-16 tablet:-ml-4">Charles A. Cameron</h3>
+                                                <p className="text-sm font-normal -ml-12 tablet:ml-0">Blue Real Esate</p>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-
-                            <div className="swiper-slide">
-                                <div className="max-w-[300px] h-[326px] tablet:ml-28 mt-10 bg-white text-slate-800 font-roboto font-medium border border-gray-50 shadow rounded-[30px] p-7 relative mr-28 -ml-12">
-                                    <div className="p-5 -mt-14 w-fit mx-auto bg-[#7666E3] text-2xl text-white rounded-full"><svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m21.95 8.721-.025-.168-.026.006A4.5 4.5 0 1 0 17.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222-.474.197.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.028 7.028 0 0 0-.063-.539zm-11 0-.025-.168-.026.006A4.5 4.5 0 1 0 6.5 14c.223 0 .437-.034.65-.065-.069.232-.14.468-.254.68-.114.308-.292.575-.469.844-.148.291-.409.488-.601.737-.201.242-.475.403-.692.604-.213.21-.492.315-.714.463-.232.133-.434.28-.65.35l-.539.222c-.301.123-.473.195-.473.195l.484 1.939.597-.144c.191-.048.424-.104.689-.171.271-.05.56-.187.882-.312.317-.143.686-.238 1.028-.467.344-.218.741-.4 1.091-.692.339-.301.748-.562 1.05-.944.33-.358.656-.734.909-1.162.293-.408.492-.856.702-1.299.19-.443.343-.896.468-1.336.237-.882.343-1.72.384-2.437.034-.718.014-1.315-.028-1.747a7.571 7.571 0 0 0-.064-.537z"></path></svg>
-                                    </div>
-
-                                    <div className="h-full flex flex-col justify-between">
-                                        <p className="pt-4 text-sm">Doing real estate business without business card was difficult for me. I found a business card template here and modified by the design owner. The designer did a fantastic job for me. Thanks...</p>
-                                        <div className="flex gap-4 pt-4">
-                                            <img alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[100px] h-[100px] rounded-full" src="https://prographr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b967d41d.png&w=640&q=75" style={{ color: 'transparent' }} />
-                                            <div className="mt-3">
-                                                <h3 className="text-lg font-semibold">Charles A. Cameron</h3>
-                                                <p className="mt-1">Blue Real Esate</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
                                 </div>
                             </div>
                         </div>
@@ -143,8 +134,8 @@ const Testimonial = () => {
                                         <img src={testimonial.image} alt="Stuff" loading="lazy" width="442" height="442" decoding="async" data-nimg="1" className="w-[100px] h-[100px] rounded-full" />
 
                                         <div className="mt-3">
-                                            <h3 className="text-lg font-semibold laptop:text-sm">Suresh Kanthaswamy</h3>
-                                            <p className="mt-1">Envelor Inc.</p>
+                                            <h3 className="text-lg font-semibold laptop:text-sm">{testimonial.name}</h3>
+                                            <p className="mt-1">{testimonial.company}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -155,7 +146,8 @@ const Testimonial = () => {
 
                 </div>
             </div>
-        </div>
+        </section>
+
     );
 };
 
