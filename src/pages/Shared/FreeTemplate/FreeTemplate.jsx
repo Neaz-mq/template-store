3dc6af1c2e19a33bb509c81928306d2d3e56f206
Neaz-mq <neazmorshed666@gmail.com>
2024-08-18 11:33:37 +0600

    FreeTemplate updated

--- src/pages/Shared/FreeTemplate/FreeTemplate.jsx
+++ src/pages/Shared/FreeTemplate/FreeTemplate.jsx
@@ -21,12 +21,12 @@ const FreeTemplate = ({ item }) => {
                             </div>
                         </div>
 
-                        <div className="flex  items-center justify-between px-5 py-4 template-details-container">
+                        <div className="flex  items-center justify-between px-5 py-4">
                             <div>
-                                <h3 className="font-bold template-title font-roboto">{name}</h3>
-                                <p className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-xs font-medium template-description">{details}</p>
+                                <h3 className="font-bold font-roboto 3xl:text-[14.5px] 2xl:text-[12px] desktop:text-[12px] laptop:text-[10px] text-[13px] -ml-5 3xl:-ml-5 2xl:-ml-4 desktop:-ml-4 laptop:-ml-4">{name}</h3>
+                                <h3 className="text-xs font-medium font-roboto 3xl:-ml-5 2xl:-ml-4 desktop:-ml-4 laptop:-ml-4 -ml-5">{details}</h3>
                             </div>
-                            <p className="font-bold text-[#2F1C6A] px-3 py-1 rounded-xl text-base bg-[#EDEEF7] template-price">{price}</p>
+                            <p className="font-bold font-roboto text-[#2F1C6A] px-3 py-1 rounded-md 3xl:text-[14px] 2xl:text-[12px] desktop:text-[10px] laptop:text-[8px] bg-[#EDEEF7] 3xl:ml-7 3xl:-mr-5 2xl:ml-7 2xl:-mr-5 desktop:ml-7 desktop:-mr-5 laptop:ml-7 laptop:-mr-5 -mr-5">$ {price}</p>
                         </div>
                     </Link>
                     
