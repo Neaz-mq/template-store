a16b2041dd7ee1496a751a1486312f73388800ad
Neaz-mq <neazmorshed666@gmail.com>
2024-08-18 11:12:32 +0600

    TemplateItem updated

--- src/pages/Home/AgencyTemplate/AgencyTemplate.jsx
+++ src/pages/Home/AgencyTemplate/AgencyTemplate.jsx
@@ -46,25 +46,24 @@ const AgencyTemplate = ({ selectedCategory, searchQuery }) => {
     return (
 
         <div className="container mx-auto">
-
-            <div className="layout lg:py-20 py-14 mt-24 lg:mx-20 ">
-                <div className="flex items-center justify-between  mb-10">
-                    <h2 className="lg:text-4xl text-xl  text-[#2F1C6A] ml-3 lg:ml-4 font-medium font-roboto 3xl:ml-[10rem] 2xl:ml-[10rem] desktop:ml-[1rem]">Top Selling <strong>Graphics Templates</strong></h2>
+            <div className="layout lg:py-20 py-14 mt-24 lg:mx-20">
+                <header className="flex items-center justify-between mb-10">
+                    <h2 className="lg:text-4xl text-lg tablet:text-xl  text-[#2F1C6A] ml-3 lg:ml-4 font-medium font-roboto 3xl:ml-[10rem] 2xl:ml-[10rem] desktop:ml-[1rem]">Top Selling <strong>Graphics Templates</strong></h2>
                     <button className="btn hidden mr-2 lg:mr-4 font-roboto text-[#47435d] bg-transparent capitalize hover:bg-primary/10 rounded-full font-semibold gap-4 shadow-none p-3 pl-4 border-slate-700">
                         <span className="-mt-1">Printing and Advertising</span>
                         <svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <path d="M277.375 85v259.704l119.702-119.702L427 256 256 427 85 256l29.924-29.922 119.701 118.626V85h42.75z"></path>
                         </svg>
                     </button>
-                </div>
-                <div className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 3xl:ml-40 3xl:mr-36 3xl:gap-x-8 3xl:gap-y-8 2xl:ml-40 2xl:mr-44 2xl:gap-x-8 2xl:gap-y-8" data-aos="fade-up" data-aos-duration="700">
+                </header>
+                <section className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 3xl:ml-40 3xl:mr-36 3xl:gap-x-4 3xl:gap-y-8 2xl:ml-40 2xl:mr-44 2xl:gap-x-4 2xl:gap-y-8 laptop:gap-x-2 laptop:gap-y-8" data-aos="fade-up" data-aos-duration="700">
                     {displayedTemplates.map(item =>
                         <TemplateItem
                             key={item._id}
                             item={item}
                         />
                     )}
-                </div>
+                </section>
             </div>
 
             <div className="flex justify-center">
--- src/pages/Shared/TemplateItem/TemplateItem.jsx
+++ src/pages/Shared/TemplateItem/TemplateItem.jsx
@@ -11,18 +11,18 @@ const TemplateItem = ({ item }) => {
             <div className="translate-y-10 opacity-0 templateScrollTrigger"
                 style={{ translate: 'none', rotate: 'none', scale: 'none', opacity: 1, transform: 'translate(0px, 0px)' }}>
                 <Link to={`/template/${_id}`}>
-                    <div className="max-w-[500px] w-[360px] h-[360px] 3xl:w-[285px] 3xl:h-[285px] 2xl:w-[250px] 2xl:h-[250px] desktop:w-[240px] desktop:h-[240px] laptop:w-[190px] laptop:h-[190px] tablet:w-[330px] tablet:h-[330px] rounded-[20px] overflow-hidden border group">
-                        <div className="p-7 bg-[#EDEEF7] overflow-hidden">
-                        <img src={image} className="w-[260px] h-[310px] 3xl:w-[180px] 3xl:h-[230px] 2xl:w-[180px] 2xl:h-[210px] desktop:w-[150px] desktop:h-[190px] laptop:w-[140px] laptop:h-[160px] tablet:w-[240px] tablet:h-[280px] object-cover group-hover:scale-105 duration-200 3xl:ml-6 2xl:ml-2 desktop:ml-4 tablet:ml-4 2xl:-mt-2 laptop:-mt-2 3xl:-mt-0 ml-5" alt={name} />
+                    <div>
+                        <div className="overflow-hidden">
+                        <img src={image} alt={name} />
                         </div>
                     </div>
 
                     <div className="flex items-center justify-between px-5 py-4">
                         <div>
-                            <h3 className="font-bold  font-roboto">{name}</h3>
-                            <p className="font-['__gellix_0bf537, __gellix_Fallback_0bf537'] text-xs font-medium">{details}</p>
+                            <h3 className="font-bold font-roboto 3xl:text-[14.5px] 2xl:text-[12px] desktop:text-[12px] laptop:text-[10px] text-[13px] -ml-5 3xl:-ml-5 2xl:-ml-4 desktop:-ml-4 laptop:-ml-4">{name}</h3>
+                            <h3 className="text-xs font-medium font-roboto 3xl:-ml-5 2xl:-ml-4 desktop:-ml-4 laptop:-ml-4 -ml-5">{details}</h3>
                         </div>
-                        <p className="font-bold font-roboto text-[#2F1C6A] px-3 py-1 rounded-xl text-base bg-[#EDEEF7]">$ {price}</p>
+                        <p className="font-bold font-roboto text-[#2F1C6A] px-3 py-1 rounded-md 3xl:text-[14px] 2xl:text-[12px] desktop:text-[10px] laptop:text-[8px] bg-[#EDEEF7] 3xl:ml-7 3xl:-mr-5 2xl:ml-7 2xl:-mr-5 ml-7 desktop:ml-7 desktop:-mr-5 laptop:ml-7 laptop:-mr-5 -mr-5">$ {price}</p>
                     </div>
 
                 </Link>
