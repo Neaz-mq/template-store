7e9d3644f8a07f7fa435d1870a29fb723c34ed5f
Neaz-mq <neazmorshed666@gmail.com>
2024-10-06 12:40:07 +0600

    working exclusivetemplate

--- src/pages/Home/Exclusive/Exclusive.jsx
+++ src/pages/Home/Exclusive/Exclusive.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import ExclusiveTemplate from "../../Shared/ExclusiveTemplate/ExclusiveTemplate";
-
-
+import { useNavigate } from "react-router-dom";
 
 const Exclusive = () => {
     const [templates, setTemplates] = useState([]);
-    
+    const navigate = useNavigate();
+
     useEffect(() => {
         // Fetch templates from JSON
         fetch('http://localhost:5000/exclusive')
@@ -15,34 +15,49 @@ const Exclusive = () => {
             })
             .catch(error => console.error("Error fetching templates:", error));  // Add error handling
     }, []);
+    
+    const template = templates.slice(0, 4);
+
+    const handleExploreMore = () => {
+        navigate("/exclusive", { state: { scrollToSection: 'graphics-template-section' } });
+    };
 
     return (
-        <div className="container mx-auto">
-           
-           <header className="layout lg:mt-2 py-12 mt-6 lg:mx-20">
-           <div className="flex items-center justify-center  mb-10">
-                    <h1 className="text-2xl tablet:text-3xl laptop:text-3xl 3xl:text-3xl 2xl:mt-12 2xl:text-3xl 3xl:mt-16 3xl:mb-10 laptop:mt-0 desktop:mt-10 tablet:mt-6 tablet:mb-6 mt-2 text-[#2F1C6A] text-center laptop:mb-4 font-roboto">
-                        Exclusive <strong>design</strong>
-                    </h1>
-                    </div>
-               
-                </header>
-
-           
-            <main className="layout lg:-mt-20 py-12 -mt-16 lg:mx-20">
-                <div className="grid grid-cols-1 mx-4 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 3xl:ml-40 3xl:mr-36 3xl:gap-x-4 3xl:gap-y-8 2xl:ml-40 2xl:mr-44 2xl:gap-x-4 2xl:gap-y-8" data-aos="fade-up" data-aos-duration="700">
-                    {templates.map(item => (
+        <div className="bg-[#ffffff] font-raleway">
+            <div className="container mx-auto 3xl:pt-5 2xl:pt-5 desktop:pt-5 laptop:pt-5 tablet:pt-5 pt-0 -mt-10 3xl:-mt-0 2xl:-mt-0 desktop:-mt-0 laptop:-mt-0 tablet:-mt-0">
+                <div className="layout lg:py-8 py-14 mt-24 lg:mx-20">
+                    <header className="flex items-center justify-between mb-10">
+                        <h1 className="text-xl tablet:text-3xl laptop:text-3xl 3xl:text-3xl 2xl:text-3xl text-[#4864EC] ml-3 lg:ml-6 font-raleway 3xl:ml-[10.6rem] 2xl:ml-[10.6rem] desktop:ml-[2rem] italic font-semibold">
+                            Exclusive <span className="not-italic font-normal 3xl:ml-2 2xl:ml-2 desktop:ml-2 laptop:ml-2 text-black">Templates</span>
+                        </h1>
+                        <button
+                            onClick={handleExploreMore}
+                            className="mr-2 3xl:mr-[8.2rem] 2xl:mr-44 desktop:mr-4 font-raleway text-[#4864EC] capitalize font-semibold gap-4 p-3 pl-4 flex items-center">
+                            <span className="-mt-1 text-[13px] 3xl:text-[15px] 2xl:text-[15px] desktop:text-[15px] laptop:text-[15px] tablet:text-[15px]">Explore more Template</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M1.5 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-.5z" />
+                                <path d="M9.646 3.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .707l-4 4a.5.5 0 0 1-.708-.707L12.793 8 9.646 4.646a.5.5 0 0 1 0-.707z" />
+                            </svg>
+                        </button>
+                    </header>
+                    <main className="layout lg:-mt-10 py-12 -mt-16 lg:mx-20">
+                        <div className="grid grid-cols-1 mx-4 md:grid-cols-2 laptop:grid-cols-2 3xl:grid-cols-4 2xl:grid-cols-4 desktop:grid-cols-4 gap-x-4 gap-y-6 3xl:ml-[5.7rem] 3xl:mr-16 3xl:gap-x-4 3xl:gap-y-8 2xl:ml-[5.7rem] 2xl:mr-24 2xl:gap-x-4 2xl:gap-y-8 desktop:-ml-12 desktop:-mr-16 laptop:-ml-12 laptop:-mr-16"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-easing="ease-in-out"
+                        >
+                            {template.map(item => (
                         <ExclusiveTemplate
                             key={item._id}
                             item={item}
                         />
                     ))}
-        </div>
-               
-            </main>
+                        </div>
+                    </main>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Exclusive;
-
--- src/pages/Home/Template/Template.jsx
+++ src/pages/Home/Template/Template.jsx
@@ -8,6 +8,7 @@ const Template = () => {
     const [counterOn, setCounterOn] = useState(false);
     const axiosSecure = useAxiosSecure();
 
+
     // Fetch the admin stats
     const { data: stats = {}, isLoading, error } = useQuery({
         queryKey: ['admin-stats'],
@@ -25,32 +26,78 @@ const Template = () => {
         return <div>Error loading stats: {error.message}</div>;
     }
 
+
+
+
     return (
         <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
-            <div className="container mx-auto overflow-x-hidden">
-                <div className="font-roboto">
-                    <section className="layout bg-[#EDEEF7] py-10 lg:py-16 rounded-[20px] mt-16 lg:mt-32 mb-20 3xl:w-[74rem] 2xl:w-[62rem] desktop:w-[65.5rem] laptop:w-[50rem] mx-auto">
-                        <div className="flex justify-center gap-4 lg:gap-36 px-5">
-                            <div className="text-center" aria-labelledby="templates-sold">
-                                <h2 id="templates-sold" className="text-2xl font-bold lg:text-5xl text-[#7666E4]">
-                                    {counterOn && <CountUp start={0} end={stats.orders || 0} duration={3} delay={0} />}
-                                </h2>
-                                <p className="text-sm text-[#7666E4]">Templates sold till now</p>
-                            </div>
-                            <div className="text-center" aria-labelledby="premium-templates">
-                                <h2 id="premium-templates" className="text-2xl font-bold lg:text-5xl text-[#7666E4]">
-                                    {counterOn && <CountUp start={0} end={stats.templates || 0} duration={3} delay={0} />}
-                                </h2>
-                                <p className="text-sm text-[#7666E4]">Premium templates</p>
-                            </div>
-                            <div className="text-center" aria-labelledby="free-templates">
-                                <h2 id="free-templates" className="text-2xl font-bold lg:text-5xl text-[#7666E4]">
-                                    {counterOn && <CountUp start={0} end={stats.free || 0} duration={3} delay={0} />}
-                                </h2>
-                                <p className="text-sm text-[#7666E4]">Free templates</p>
+            <div className="overflow-hidden font-raleway">
+                <div className="bg-[#4864EC] h-[14.5rem] w-7 3xl:block 2xl:block desktop:block laptop:block tablet:hidden hidden"></div>
+                <div className="bg-[#ffffff] container mx-auto">
+                    <div className="flex">
+                        <div className="flex 3xl:ml-60 2xl:ml-60 desktop:ml-24 tablet:ml-[16rem] laptop:ml-24 ml-16">
+                            <div>
+                                <h1 className="3xl:text-4xl 2xl:text-4xl desktop:text-4xl laptop:text-4xl text-2xl font-semibold text-left leading-[2.5rem] text-[#4864EC] font-raleway 3xl:-mt-44 2xl:-mt-44 desktop:-mt-44 laptop:-mt-44 hidden 3xl:block 2xl:block desktop:block laptop:block">
+                                    Explore <br /> <span className="italic">our work</span>
+                                </h1>
+                                <h1 className="text-[#4864EC] 3xl:hidden 2xl:hidden desktop:hidden laptop:hidden block tablet:text-3xl text-2xl font-raleway font-semibold tablet:mt-16 mt-12 -ml-3 tablet:-ml-0">
+                                    Explore our work
+                                </h1>
+                                <p className="font-raleway text-sm mt-3 text-[#282A37] ml-1 hidden 3xl:block 2xl:block desktop:block laptop:block">
+                                    Discover graphic design templates <br /> to enhance your brand's look.
+                                </p>
                             </div>
                         </div>
-                    </section>
+                    </div>
+                    <div className="bg-[#4864EC] h-[21.5rem] 3xl:h-[14.5rem] 2xl:h-[14.5rem] desktop:h-[14.5rem] laptop:h-[14.5rem] tablet:h-[13rem] w-fit 3xl:w-screen 2xl:w-screen desktop:w-screen laptop:w-screen overflow-hidden tablet:overflow-hidden container mx-auto 3xl:-mt-56 3xl:ml-[36rem] 2xl:-mt-56 2xl:ml-[36rem] desktop:-mt-56 laptop:-mt-56 tablet:mt-14 mt-10 desktop:ml-[30rem] laptop:ml-[22rem] tablet:ml-[0.5rem] ml-3">
+
+                        <div className="font-raleway">
+                            <section className="text-white">
+                                <div className="flex justify-start items-start flex-col 3xl:flex-row 2xl:flex-row desktop:flex-row laptop:flex-row tablet:flex-row gap-2 lg:gap-16 3xl:gap-28 2xl:gap-24 desktop:gap-20 tablet:gap-16 px-5 mt-6 3xl:ml-24 3xl:mt-7 2xl:ml-20 desktop:ml-8 laptop:ml-0 tablet:ml-6 ml-2">
+                                    <div className="text-center">
+                                        <h1 className="text-2xl font-bold lg:text-5xl 3xl:mt-8 2xl:mt-8 desktop:mt-8 laptop:mt-8 tablet:mt-8 mt-2 -ml-6 3xl:-ml-0" aria-label="Template sales">
+                                            {counterOn && <CountUp start={0} end={stats.orders || 0} duration={3} delay={0} />}
+                                        </h1>
+                                        <p className="text-lg text-white mt-5 ml-6 hidden 3xl:block 2xl:block desktop:block laptop:block">
+                                            Template <br /> <span className="ml-6">sold till now</span>
+                                        </p>
+                                        <p className="text-lg text-white mt-5 block 3xl:hidden 2xl:hidden desktop:hidden laptop:hidden">
+                                            Template sold till now
+                                        </p>
+                                    </div>
+                                    <div className="text-center ml-4">
+                                        <h1 className="text-2xl font-bold lg:text-5xl 3xl:mt-8 2xl:mt-8 desktop:mt-8 laptop:mt-8 tablet:mt-8 mt-2 -ml-9" aria-label="Premium templates">
+                                            {counterOn && <CountUp start={0} end={stats.templates || 0} duration={3} delay={0} />}
+                                        </h1>
+                                        <p className="text-lg text-white mt-5 ml-6 mr-12 hidden 3xl:block 2xl:block desktop:block laptop:block">
+                                            Premium <br /> <span className="ml-2">Templates</span>
+                                        </p>
+                                        <p className="text-lg text-white mt-5 block 3xl:hidden 2xl:hidden desktop:hidden laptop:hidden">
+                                            Premium Templates
+                                        </p>
+                                    </div>
+                                    <div className="text-center ml-9">
+                                        <h1 className="text-2xl font-bold lg:text-5xl 3xl:mt-8 2xl:mt-8 desktop:mt-8 laptop:mt-8 tablet:mt-8 mt-2 3xl:-ml-9 2xl:-ml-9 desktop:-ml-9 laptop:-ml-9 tablet:-ml-0 -ml-9" aria-label="Free templates">
+                                            {counterOn && <CountUp start={0} end={stats.free || 0} duration={3} delay={0} />}
+                                        </h1>
+                                        <p className="text-lg text-white mt-5  hidden 3xl:block 2xl:block desktop:block laptop:block  -ml-12">
+                                            Free <br /> <span className="ml-10">Templates</span>
+                                        </p>
+                                        <p className="text-lg text-white mt-5 block 3xl:hidden 2xl:hidden desktop:hidden laptop:hidden">
+                                            Free Templates
+                                        </p>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+
+                        <img
+                            className="3xl:-mt-[15rem] 3xl:-ml-[12rem] 2xl:-mt-[16rem] 2xl:-ml-[10rem] desktop:-mt-[12rem] desktop:-ml-[11rem] laptop:-mt-[12rem] laptop:-ml-[8rem] tablet:-mt-[10.5rem] tablet:-ml-[7rem] -mt-[12rem] opacity-45"
+                            src="/Line Art.svg"
+                            alt="Line Art"
+                            loading="lazy"
+                        />
+                    </div>
                 </div>
             </div>
         </ScrollTrigger>
