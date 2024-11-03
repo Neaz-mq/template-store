850059247155fafa0edca9ec6f15c78b138396ac
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2024-11-03 14:39:25 +0600

    Template download element working

--- src/pages/Dashboard/TemplateDownload/TemplateDownload.jsx
+++ src/pages/Dashboard/TemplateDownload/TemplateDownload.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 const TemplateDownload = () => {
     return (
         <div>
-            <h1>Download Template</h1>
+
+            <h1 className='text-4xl'>Download Template</h1>
+           
+           <div className="overflow-x-auto w-full lg:w-full mt-16">
+
+                <table className="hidden lg:table table-zebra">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Type</th>
+                            <th>Price</th>
+                           
+                        </tr>
+                    </thead>
+                   
+                </table>
+
+               
+            </div>
         </div>
     );
 };
