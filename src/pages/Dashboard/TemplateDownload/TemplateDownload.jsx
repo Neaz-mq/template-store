0fa2c27e61881ca9d24e81073a3133d7a0574298
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2024-11-04 12:23:57 +0600

    Template download element working

--- src/pages/Dashboard/TemplateDownload/TemplateDownload.jsx
+++ src/pages/Dashboard/TemplateDownload/TemplateDownload.jsx
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import useAuth from "../../../hooks/useAuth";
-import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 
 const TemplateDownload = () => {
-    const { user } = useAuth(); // Access logged-in user's information
-    const axiosSecure = useAxiosSecure();
-    const [purchasedTemplates, setPurchasedTemplates] = useState([]);
+   
 
-    useEffect(() => {
-        // Fetch purchased templates for the logged-in user
-        const fetchPurchasedTemplates = async () => {
-            try {
-                const response = await axiosSecure.get(`/purchased-templates/${user.email}`);
-                setPurchasedTemplates(response.data);
-            } catch (error) {
-                console.error("Error fetching purchased templates:", error);
-            }
-        };
-
-        fetchPurchasedTemplates();
-    }, [axiosSecure, user.email]);
 
     return (
         <div>
@@ -35,16 +18,7 @@ const TemplateDownload = () => {
                             <th>Price</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {purchasedTemplates.map((template, index) => (
-                            <tr key={template._id}>
-                                <td>{index + 1}</td>
-                                <td>{template._id}</td>
-                                <td>{template.type}</td>
-                                <td>${parseFloat(template.amount).toFixed(2)}</td>
-                            </tr>
-                        ))}
-                    </tbody>
+                  
                 </table>
             </div>
         </div>
