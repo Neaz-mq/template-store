52262a6f0f5bb4622d17b01cc7e8be721d34132f
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2025-01-02 15:59:10 +0600

    updated some home page section

--- src/pages/Home/Promote/Promote.jsx
+++ src/pages/Home/Promote/Promote.jsx
@@ -1,8 +1,8 @@
 const Promote = () => {
     return (
-      <div className="bg-[#4864EC] h-[14.5rem] w-auto -mt-14 overflow-hidden font-raleway">
+      <div className="bg-[#4864EC] h-[14.5rem] w-auto 3xl:-mt-14 2xl:-mt-14 desktop:-mt-14 laptop:-mt-14 tablet:-mt-14 -mt-32 overflow-hidden font-raleway">
         <div className="container mx-auto">
-          <div className="relative flex flex-col 3xl:flex-row 2xl:flex-row desktop:flex-row laptop:flex-row tablet:flex-row items-center justify-start 3xl:ml-[15.8rem] 2xl:ml-[15.8rem] desktop:ml-[7.3rem] laptop:ml-[7.3rem] tablet:ml-[1.3rem]">
+          <div className="relative flex flex-col 3xl:flex-row 2xl:flex-row desktop:flex-row laptop:flex-row tablet:flex-row items-center justify-start 3xl:ml-[14.8rem] 2xl:ml-[14.8rem] desktop:ml-[6.5rem] laptop:ml-[6rem] tablet:ml-0">
             <div>
               <h1 className="3xl:text-3xl 2xl:text-3xl desktop:text-3xl laptop:text-3xl text-2xl font-semibold text-left 3xl:leading-[2.8rem] 2xl:leading-[2.8rem] desktop:leading-[2.8rem] laptop:leading-[2.8rem] leading-[2.3rem] text-white font-raleway 3xl:-mt-20 2xl:-mt-12 desktop:-mt-2 laptop:mt-8 tablet:mt-16 mt-6 ml-4">
                 Looking to <br /> <span className="italic">promote</span> <br /> <span>your business?</span>
