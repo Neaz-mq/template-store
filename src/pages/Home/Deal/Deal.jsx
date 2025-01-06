6e12b81d3f10b56a39ce3d2372c79b33be530265
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2025-01-06 15:05:55 +0600

    deal work done

--- src/pages/Home/Deal/Deal.jsx
+++ src/pages/Home/Deal/Deal.jsx
@@ -21,7 +21,7 @@ const Deal = () => {
         // Toggle between first and second content every 5 seconds
         const interval = setInterval(() => {
             setShowFirstContent((prev) => !prev); // Toggle content
-        }, 5000); // Change every 5 seconds
+        }, 4000); // Change every 5 seconds
 
         return () => clearInterval(interval); // Cleanup interval on unmount
     }, []);
@@ -61,7 +61,7 @@ const Deal = () => {
                                                 {deal.paragraph}
                                             </h1>
                                             <p
-                                                className="3xl:text-lg 2xl:text-lg desktop:text-base laptop:text-sm text-sm tablet:text-sm 3xl:ml-[4rem] 3xl:-mr-8 2xl:ml-[14.2rem] 2xl:-mr-8 desktop:ml-[5rem] desktop:-mr-8 laptop:ml-[6.3rem] laptop:-mr-2  font-medium  tablet:leading-[30px] text-center lg:text-start 3xl:mt-2 2xl:mt-2 desktop:mt-2 laptop:mt-2 mb-3 font-raleway tablet:mt-0 mt-2"
+                                                className="3xl:text-lg 2xl:text-lg desktop:text-base laptop:text-sm text-sm tablet:text-sm 3xl:ml-[5.2rem] 3xl:-mr-8 2xl:ml-[14.2rem] 2xl:-mr-8 desktop:ml-[5rem] desktop:-mr-8 laptop:ml-[6.3rem] laptop:-mr-2  font-medium  tablet:leading-[30px] text-center lg:text-start 3xl:mt-2 2xl:mt-2 desktop:mt-2 laptop:mt-2 mb-3 font-raleway tablet:mt-0 mt-2"
                                                 style={{ color: deal.variant }} // Apply variant to summary
                                             >
                                                 {deal.summary}
@@ -72,7 +72,7 @@ const Deal = () => {
                                     {/* More Template Button */}
                                     <a href="/template">
                                         <button
-                                            className="mt-4 px-8 py-3 bg-[#4864EC] text-white font-bold rounded-lg transition duration-300 ease-in-out focus:outline-none 3xl:ml-4 2xl:ml-5 desktop:-ml-[6.5rem] laptop:-ml-12"
+                                            className="mt-4 px-8 py-3 bg-[#4864EC] text-white font-bold rounded-lg transition duration-300 ease-in-out focus:outline-none 3xl:ml-9 2xl:ml-5 desktop:-ml-[6.5rem] laptop:-ml-12"
                                         >
                                             More Template
                                         </button>
@@ -80,11 +80,11 @@ const Deal = () => {
                                 </div>
 
                                 {/* Right Section: Image */}
-                                <div className="w-full lg:w-1/2 relative">
+                                <div className="w-full lg:w-[60%] relative">
                                     <img
                                         src={showFirstContent ? deal.image : deal.photo} // Switch images based on content
                                         alt={`Deal ${index + 1}`}
-                                        className="3xl:w-[40.2rem] 3xl:h-[50rem] 2xl:w-[36rem] 2xl:h-[48rem] desktop:w-[39rem] desktop:h-[48rem] laptop:w-[30rem] laptop:h-[45rem] tablet:w-[30rem] tablet:h-[45rem] object-cover 3xl:ml-20 2xl:-ml-8 desktop:ml-1 tablet:ml-32 laptop:-ml-20 3xl:-mt-0 2xl:-mt-0 desktop:-mt-0 laptop:-mt-0 tablet:-mt-20 tablet:pb-20 3xl:pb-0 2xl:pb-0 desktop:pb-0 laptop:pb-0 -mt-14 pb-20 w-[30rem] h-[30rem]"
+                                        className="3xl:w-[44rem] 3xl:h-[50rem] 2xl:w-[36rem] 2xl:h-[48rem] desktop:w-[39rem] desktop:h-[48rem] laptop:w-[30rem] laptop:h-[45rem] tablet:w-[30rem] tablet:h-[45rem] object-cover 3xl:ml-10 2xl:-ml-8 desktop:ml-1 tablet:ml-32 laptop:-ml-20 3xl:-mt-0 2xl:-mt-0 desktop:-mt-0 laptop:-mt-0 tablet:-mt-20 tablet:pb-20 3xl:pb-0 2xl:pb-0 desktop:pb-0 laptop:pb-0 -mt-14 pb-20 w-[30rem] h-[30rem]"
                                     />
                                 </div>
                             </div>
