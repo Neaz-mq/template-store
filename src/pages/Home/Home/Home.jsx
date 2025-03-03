b5f085838c01001218aeba34b5142e68fa806382
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2025-03-03 13:15:21 +0600

    work chatbot

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,47 +1,57 @@
-import { Helmet } from 'react-helmet-async';
-import Community from '../Community/Community';
-import Template from '../Template/Template';
-import Exclusive from '../Exclusive/Exclusive';
-import Free from '../Free/Free';
-import Promote from '../Promote/Promote';
-import Package from '../Package/Package';
-import Deal from '../Deal/Deal';
+import { Helmet } from "react-helmet-async";
+import Community from "../Community/Community";
+import Template from "../Template/Template";
+import Exclusive from "../Exclusive/Exclusive";
+import Free from "../Free/Free";
+import Promote from "../Promote/Promote";
+import Package from "../Package/Package";
+import Deal from "../Deal/Deal";
+import Chatbot from "../../Chatbot/Chatbot";
+
 
 const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Prographr | Home</title>
+        <meta
+          name="description"
+          content="Discover a wide range of templates for your creative projects at Template Store. Explore community ideas, guidelines, testimonials, and more."
+        />
+        <meta
+          name="keywords"
+          content="templates, creativity, community, guidelines, ideas, testimonials"
+        />
+        <link rel="canonical" href="https://www.prographr.com/home" />
+      </Helmet>
+      <main>
+        <section id="deal">
+          <Deal />
+        </section>
+        <section id="templates">
+          <Template />
+        </section>
+        <section id="exclusive">
+          <Exclusive />
+        </section>
+        <section id="free">
+          <Free />
+        </section>
+        <section id="promote">
+          <Promote />
+        </section>
+        <section id="package">
+          <Package />
+        </section>
+        <section id="community">
+          <Community />
+        </section>
+      </main>
 
-    return (
-        <div>
-            <Helmet>
-                <title>Prographr | Home</title>
-                <meta name="description" content="Discover a wide range of templates for your creative projects at Template Store. Explore community ideas, guidelines, testimonials, and more." />
-                <meta name="keywords" content="templates, creativity, community, guidelines, ideas, testimonials" />
-                <link rel="canonical" href="https://www.prographr.com/home" />
-            </Helmet>
-            <main>
-                <section id="deal">
-                   <Deal />
-                </section>
-                <section id="templates">
-                    <Template />
-                </section>             
-                <section id="exclusive">
-                    <Exclusive />
-                </section>             
-                <section id="free">
-                    <Free />
-                </section> 
-                <section id="promote">
-                    <Promote />
-                </section>             
-                <section id="package">
-                    <Package />
-                </section>            
-                <section id="community">
-                    <Community />
-                </section>
-            </main>
-        </div>
-    );
+      {/* Chatbot in the bottom-right corner */}
+      <Chatbot></Chatbot>
+    </div>
+  );
 };
 
 export default Home;
