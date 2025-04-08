c90123d60ba413373d495aee3081ec565b25f8fe
Neaz-mq <neazmorshed666@gmail.com>
2025-04-08 17:59:54 +0600

    update headbar

--- src/pages/Shared/HeadBar/HeadBar.jsx
+++ src/pages/Shared/HeadBar/HeadBar.jsx
@@ -3,18 +3,9 @@ const HeadBar = () => {
     <div className="w-full">
       <div className="w-full overflow-hidden">
         <div className="flex justify-center">
-          {/* Marquee for the development text */}
           <div className="w-full">
             <p
-              className="inline-block whitespace-nowrap text-stone-50 3xl:px-4 3xl:py-4 2xl:py-2 desktop:py-3 laptop:py-4 tablet:py-4 py-4  
-                font-semibold 
-                animate-marquee 
-                sm:animate-marquee-sm 
-                tablet:animate-marquee-tablet 
-                laptop:animate-marquee-laptop 
-                desktop:animate-marquee-desktop 
-                2xl:animate-marquee-2xl 
-                3xl:animate-marquee"
+              className="text-sm text-stone-50 font-semibold px-4 py-4 text-center"
             >
               The Web Application is under Development. You can try for demo use. Our Application will be fully functional very soon. Keep patience and stay with us. We have a dedicated team
               members specialized in specific sectors, can be logo specialists, or print design specialists.
