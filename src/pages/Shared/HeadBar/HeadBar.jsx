413c2320add5236bf7e9b72aea89d0233d39a219
Neaz-mq <neazmorshed666@gmail.com>
2025-04-09 17:49:17 +0600

    headbar work

--- src/pages/Shared/HeadBar/HeadBar.jsx
+++ src/pages/Shared/HeadBar/HeadBar.jsx
@@ -1,14 +1,13 @@
 const HeadBar = () => {
   return (
-    <div className="w-full hidden 3xl:block 2xl:block desktop:block laptop:block">
+    <div className="w-full hidden 3xl:block 2xl:block desktop:block laptop:block bg-blue-500"> {/* Added bg-blue-500 class */}
       <div className="w-full overflow-hidden">
         <div className="flex justify-center">
           <div className="w-full">
             <p
               className="text-sm text-stone-50 font-semibold px-4 py-4 text-center"
             >
-              The Web Application is under Development. You can try for demo use. Our Application will be fully functional very soon. Keep patience and stay with us. We have a dedicated team
-              members specialized in specific sectors, can be logo specialists, or print design specialists.
+              The Web Application is under Development. You can try for demo use. Our Application will be fully functional very soon. Keep patience and stay with us.
             </p>
           </div>
         </div>
