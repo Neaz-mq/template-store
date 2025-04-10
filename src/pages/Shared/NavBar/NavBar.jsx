7d27ae3d1ed23a3e676e94e78556f7c11a3fb0c7
Neaz-mq <neazmorshed666@gmail.com>
2025-04-10 15:34:28 +0600

    work navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -74,7 +74,7 @@ const NavBar = () => {
               </div>
               <ul
                 tabIndex={0}
-                className="menu menu-sm cursor-pointer dropdown-content -mt-12 z-[1] p-8 leading-8 shadow bg-base-100 w-52 text-center -ml-4 font-raleway font-semibold text-[17px]"
+                className="menu menu-sm cursor-pointer dropdown-content -mt-12 z-[1] p-8 leading-8 shadow bg-base-100 w-52 text-center -ml-4 font-raleway font-semibold text-[15px]"
               >
                 <Link to="/" className="text-xl mt-5 ml-4 mr-1 mb-8">
                   <img
@@ -174,7 +174,7 @@ const NavBar = () => {
 
           {/* Center menu */}
           <div className="navbar-center hidden 3xl:flex 2xl:flex desktop:flex ml-16 -mr-14 3xl:mr-36 3xl:-mt-28 2xl:-mt-28 desktop:-mt-28">
-            <ul className="menu cursor-pointer menu-horizontal text-[#ffffff] flex-row gap-8 font-raleway font-medium 3xl:mr-8 3xl:gap-8 3xl:mt-[5rem] 2xl:mr-28 2xl:gap-0 desktop:gap-4 desktop:mt-[5.2rem] 2xl:mt-[5rem] desktop:ml-16 text-[17px] laptop:mr-1 laptop:ml-32 laptop:gap-1 laptop:mt-[5.5rem]">
+            <ul className="menu cursor-pointer menu-horizontal text-[#ffffff] flex-row gap-8 font-raleway font-medium 3xl:mr-8 3xl:gap-8 3xl:mt-[5.2rem] 2xl:mr-28 2xl:gap-0 desktop:gap-4 desktop:mt-[5.2rem] 2xl:mt-[5rem] desktop:ml-16 text-[15px] laptop:mr-1 laptop:ml-32 laptop:gap-1 laptop:mt-[5.5rem]">
               <Link
                 to="/template"
                 onClick={() => handleSelect('template')}
@@ -217,11 +217,11 @@ const NavBar = () => {
           {/* Right section (Sign in / Sign up / Sign out) */}
           <div className="navbar-end gap-3 3xl:gap-0 2xl:gap-0 desktop:gap-0">
             {user ? (
-              <div className="3xl:-ml-52 3xl:mr-56 2xl:-ml-52 2xl:mr-60 desktop:-ml-60 desktop:mr-16 laptop:-ml-60 laptop:mr-12 tablet:-mt-[5.3rem] 3xl:-mt-0 2xl:-mt-0 desktop:-mt-0 laptop:-mt-0 tablet:-ml-72 tablet:mr-20  -mt-24">
+              <div className="3xl:-ml-52 3xl:mr-56 2xl:-ml-52 2xl:mr-60 desktop:-ml-60 desktop:mr-16 laptop:-ml-60 laptop:mr-12 tablet:-mt-32 3xl:-mt-12 2xl:-mt-12 desktop:-mt-12 laptop:-mt-12 tablet:-ml-72 tablet:mr-20  -mt-32">
                 {user.photoURL && user.photoURL !== '' ? (
                   <img
                     src={user.photoURL}  // Use photoURL if it exists and is not empty
-                    className="w-12 h-12 rounded-full cursor-pointer mt-24"
+                    className="w-12 h-12 rounded-full cursor-pointer mt-10"
                     onClick={() => setDropdownOpen((prev) => !prev)}
                   />
                 ) : (
@@ -260,8 +260,8 @@ const NavBar = () => {
             ) : (
               <div className="flex items-center">
                 <Link to="/sign-in" className="mr-4 3xl:-mt-28 2xl:-mt-28 desktop:-mt-28 laptop:-mt-28 tablet:-mt-24 -mt-24">
-                  <button className="capitalize font-raleway font-medium  text-[#ffffff] gap-4 shadow-none 3xl:px-7 2xl:px-7 desktop:px-7 px-5 -ml-16   mr-8 3xl:py-5 2xl:py-5 desktop:py-5 py-[1.12rem] 3xl:mr-44  2xl:mr-60 2xl:mt-[6.3rem] desktop:mr-20 desktop:mt-[6.4rem] laptop:mt-[6.4rem] tablet:mr-10">
-                    <span className="-mt-2">Sign in</span>
+                  <button className="capitalize font-raleway font-medium  text-[#ffffff] gap-4 shadow-none 3xl:px-7 2xl:px-7 desktop:px-7 px-5 -ml-16   mr-8 3xl:py-5 2xl:py-5 desktop:py-5 py-[1.12rem] 3xl:mr-44  2xl:mr-60 2xl:mt-[6.3rem] desktop:mr-20 desktop:mt-[6.4rem] laptop:mt-[6.4rem] tablet:mr-10 ">
+                    <span className="-mt-2 text-[15px]">Sign in</span>
                   </button>
                 </Link>
               </div>
@@ -272,7 +272,7 @@ const NavBar = () => {
               <div className="flex items-center">
                 <Link to="/sign-up" className='tablet:-mt-24 -mt-24 3xl:-mt-0 2xl:-mt-0 desktop:-mt-0 laptop:-mt-0'>
                   <button className=" hover:bg-[#4864EC] bg-[#4864EC] font-raleway rounded-none capitalize text-white gap-4 3xl:px-4 2xl:px-4 desktop:px-4 px-4 3xl:py-2 2xl:py-2 desktop:py-2 py-2 -mr-4 -ml-14 3xl:mr-56 3xl:-ml-48  2xl:mr-[15.5rem] 2xl:-ml-[15.6rem]  desktop:mr-[4.5rem] desktop:-ml-20 laptop:mr-14 laptop:-ml-10 tablet:mr-10 font-medium 3xl:-mt-36 2xl:-mt-36 desktop:-mt-36 laptop:-mt-36   ">
-                    <span className="-mt-2">Sign Up</span>
+                    <span className="-mt-2 text-[15px]">Sign Up</span>
                   </button>
                 </Link>
               </div>
