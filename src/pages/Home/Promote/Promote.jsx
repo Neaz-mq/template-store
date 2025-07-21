c6676a700936b5a820807b6190cd535bdd7f8a53
Neaz-mq <neazmorshed666@gmail.com>
2025-07-21 12:15:55 +0600

    update the promote

--- src/pages/Home/Promote/Promote.jsx
+++ src/pages/Home/Promote/Promote.jsx
@@ -17,7 +17,7 @@ const Promote = () => {
             />
           </div>
 
-          <a className="3xl:-mt-24 2xl:-mt-16 desktop:-mt-2 laptop:mt-6 tablet:mt-14 relative z-10" href="/template">
+          <a className="3xl:-mt-24 2xl:-mt-16 desktop:-mt-2 laptop:mt-6 tablet:mt-14 relative z-10" href="https://graphicriver.net/user/prographr/portfolio">
             <button className="bg-white text-[#282A37] 3xl:py-3 3xl:px-10 2xl:py-3 2xl:px-10 desktop:py-3 desktop:px-10 laptop:py-3 laptop:px-10 tablet:py-2 tablet:px-8 py-2 px-6 font-bold 3xl:-ml-[46rem] 2xl:-ml-[42rem] desktop:-ml-[33rem] laptop:-ml-[30rem] tablet:ml-44 mt-6 whitespace-nowrap">
               Get your template now
             </button>
